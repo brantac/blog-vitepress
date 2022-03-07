04c061ced338faa7251acc6ec3ec06a16c7b3898
Arthur Brant <arthurcl.br@hotmail.com>
2022-03-07 20:02:05 -0300

    add an introduction text

--- docs/.vitepress/theme/Home.vue
+++ docs/.vitepress/theme/Home.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-        <h1>Home Page</h1>
-        <a href="../posts/2022/02/16/data-structures-in-javascript/">Data Structures In Javascript</a>
+    <div class="blog-introduction flex">
+        <h1 class="title">Read Topics About</h1>
+        <div class="tags flex">
+            <span class="tag-item">#softwareengineer</span>
+            <span class="tag-item">#tutorials</span>
+            <span class="tag-item">#tips</span>
+        </div>
     </div>
+    
+    <a href="../posts/2022/02/16/data-structures-in-javascript/">Data Structures In Javascript</a>
 </template>
 
 <script setup lang="ts">
@@ -10,5 +16,11 @@
 </script>
 
 <style scoped>
-
+.blog-introduction {
+    width: 100%;
+    flex-direction: column;
+}
+.blog-introduction h1 {
+    text-align: center;
+}
 </style>
