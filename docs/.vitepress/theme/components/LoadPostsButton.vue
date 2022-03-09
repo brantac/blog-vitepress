25bc19f8f4c5c000844067a66c4e122a8ba8c01b
Arthur Brant <arthurcl.br@hotmail.com>
2022-03-09 18:02:28 -0300

    style the button component

---
+++ docs/.vitepress/theme/components/LoadPostsButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="load-posts-button" @click="loadPosts">Load more posts</button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.load-posts-button {
+    width: 100%;
+    height: 50px;
+    background-color: #645dd7;
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    margin-bottom: 6rem;
+    border-radius: 0.5rem;
+}
+</style>
