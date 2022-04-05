e22d3e6a3b4006958334589cf4d3212d856b589e
Arthur Brant <arthurcl.br@hotmail.com>
2022-04-05 17:23:15 -0300

    increase the amount of custom spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ["./docs/.vitepress/theme/**/*.vue"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        /**
+         * cs: custom space
+         * The scale was calculated using the golden ratio (1.618)
+        */
+        'cs-0': '2.62px',
+        'cs-1': '4.24px',
+        'cs-2': '6.87px',
+        'cs-3': '11.12px',
+        'cs-4': '18px',   // This is the base scale
+        'cs-5': '29px',
+        'cs-6': '47px',
+        'cs-7': '76px',
+        'cs-8': '123px',
+        'cs-9': '199px',
+        'cs-10': '322px',
+        'cs-11': '522px',
+        'cs-12': '845px',
+        'cs-13': '1367px',
+        'cs-14': '2213px',
+      }
+    },
   },
   plugins: [],
 }
