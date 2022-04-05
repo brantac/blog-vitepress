b5f6e72d8fddd376e1be0114b4d5a914ef39b9a8
Arthur Brant <arthurcl.br@hotmail.com>
2022-04-05 17:49:13 -0300

    added tailwind, improved Typography and spacing

--- docs/.vitepress/theme/Home.vue
+++ docs/.vitepress/theme/Home.vue
@@ -1,32 +1,40 @@
 <template>
-    <div class="blog-introduction flex">
-        <h1 class="introduction-title">Read Topics About</h1>
-        <div class="tags flex">
+    <div class="blog-introduction flex flex-col content-start
+    w-full px-cs-4 md:px-cs-8 lg:px-cs-9 xl:px-cs-10 items-start
+    pt-cs-6 md:pt-cs-7 lg:pt-cs-8
+    pb-cs-6 md:pb-cs-7 lg:pb-cs-8">
+        <h1 class="font-black">Blog</h1>
+        <h2 class="introduction-title">Read Topics About</h2>
+        <div class="tag-container">
             <span class="tag-item">#softwareengineer</span>
             <span class="tag-item">#tutorials</span>
             <span class="tag-item">#tips</span>
         </div>
     </div>
+    <!-- <main class="mx-auto px-cs-4 lg:px-0 py-16 max-w-3xl"> -->
+    <main class="px-cs-4 md:px-cs-8 lg:px-cs-9 xl:px-cs-10 pb-16">
+        <div class="posts">
+            <div class="post-item" v-for="{ title, href, date, excerpt } of posts">
+                <div class="post-date">{{ date.string }}</div>
+                <h3 class="title font-bold">
+                    <a :href="href">{{ title }}</a>
+                </h3>
+                <div class="tag-container">
+                    <span class="tag-item">#softwareengineer</span>
+                </div>
+                <div class="excerpt">{{ excerpt }}</div>
+                <div class="read-the-article">
+                    <a :href="href">Read the article →</a>
+                </div>
     
-    <div class="posts">
-        <div class="post-item" v-for="{ title, href, date, excerpt } of posts">
-            <div class="post-date">{{ date.string }}</div>
-            <h2 class="title">
-                <a :href="href">{{ title }}</a>
-            </h2>
-            <div class="tags flex">
-                <span class="tag-item">#softwareengineer</span>
+                <hr>
             </div>
-            <div class="excerpt">{{ excerpt }}</div>
-            <div class="read-the-article">
-                <a :href="href">Read the article →</a>
-            </div>
-
-            <hr>
         </div>
-    </div>
-
-    <button class="load-posts-button" @click="">Load more posts</button>
+    
+        <div class="load_posts_container mt-cs-6 w-full text-center">
+            <button class="load-posts-button" @click="">Load more posts</button>
+        </div>
+    </main>
 </template>
 
 <script setup lang="ts">
@@ -35,47 +43,18 @@ import { data as posts } from '../posts.data'
 </script>
 
 <style scoped>
-.blog-introduction {
-    width: 100%;
-    flex-direction: column;
-}
-.blog-introduction h1 {
-    text-align: center;
-}
-.blog-introduction .tags {
-    justify-content: center;
-}
-.introduction-title {
-    font-family: 'Work Sans', sans-serif;
-    font-weight: 900;
-}
-.posts {
-    margin: 6rem 0;
-}
 .post-item:not(:last-child) {
     margin-bottom: 2.5rem;
 }
 .posts hr {
     margin-top: 2.5rem;
 }
-.post-item .tags {
-    width: 100%;
-    flex-direction: row;
-    justify-content: start;
-    margin-bottom: 1rem;
-}
-.post-item .tag-item:first-child {
-    margin-left: 0;
-}
-.title {
-    margin: 0.5rem 0;
-}
 .title a {
-    color: #000;
+    color: var(--text-color);
     text-decoration: none;
 }
 .read-the-article a {
-    color: #645dd7;
+    color: var(--link-color);
 }
 .excerpt {
     margin-bottom: 1rem;
