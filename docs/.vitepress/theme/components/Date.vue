df173191e0319c7d80278932c9ce84e12cecefde
Arthur Brant <arthurcl.br@hotmail.com>
2022-04-11 13:56:05 -0300

    show custom date passed through props

---
+++ docs/.vitepress/theme/components/Date.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+    date: Object
+}>();
+
+function getDateTime() {
+    return new Date(props.date.time).toISOString();
+}
+</script>
+
+<template>
+    <dl>
+        <dt class="sr-only">Date</dt>
+        <dd>
+            <time :datetime="getDateTime()">{{ date.string }}</time>
+        </dd>
+    </dl>
+</template>
+
+<style scoped>
+
+</style>
