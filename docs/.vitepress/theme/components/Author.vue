ad340cf96e7c91e055da838f19fbbf1056d091b3
Arthur Brant <arthurcl.br@hotmail.com>
2022-05-18 14:52:16 -0300

    bring author div from Post.vue

---
+++ docs/.vitepress/theme/components/Author.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Socials from "./Socials.vue";
+</script>
+
+<template>
+    <div class="author flex flex-row gap-x-4">
+        <div class="name ">Arthur Brant</div>
+        <Socials />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
