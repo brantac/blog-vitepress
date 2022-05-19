48c5846bd9cca65941afdd66b517bb6d2dcb2720
Arthur Brant <arthurcl.br@hotmail.com>
2022-05-19 14:18:21 -0300

    replace share icon by the Share component

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -20,13 +20,7 @@
             </a>
         </div>
         <div id="navbar-menu" class="flex flex-row align-center gap-cs-4">
-            <span class="share-blog-btn">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20.315" height="24" viewBox="0 0 20.315 26">
-                    <g id="Sharing_Button" data-name="Sharing Button" transform="translate(1 1)">
-                        <path id="Sharing_Button-2" data-name="Sharing Button" d="M10.5,20.09a3.9,3.9,0,0,1,.795-2.365L6.986,14.4a3.91,3.91,0,1,1,.127-4.635l4.32-3.327A3.9,3.9,0,1,1,14.41,7.82a3.9,3.9,0,0,1-2.977-1.379L7.113,9.768A3.915,3.915,0,0,1,6.986,14.4L11.3,17.725A3.911,3.911,0,1,1,10.5,20.09Z" fill="none" stroke="#313628" stroke-width="2"/>
-                    </g>
-                </svg>
-            </span>
+            <Share />
             <span class="menu-toggler">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 34 34">
                     <g id="Hamburger_Menu_Open" data-name="Hamburger Menu Open" transform="translate(1 1)">
@@ -52,6 +46,7 @@ import { computed, ref } from 'vue'
 import Socials from './components/Socials.vue'
 import Home from './Home.vue'
 import Post from './Post.vue'
+import Share from './components/Share.vue'
 
 const route = useRoute()
 const isHome = computed(() => route.path.replace(/index.html$/, '') === '/')
@@ -64,8 +59,7 @@ const isHome = computed(() => route.path.replace(/index.html$/, '') === '/')
 .logo a {
     text-decoration: none;
 }
-.menu-toggler,
-.share-blog-btn {
+.menu-toggler {
     cursor: pointer;
 }
 #Brand_SVG_Optimized {
