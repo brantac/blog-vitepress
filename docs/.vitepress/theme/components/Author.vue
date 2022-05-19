b3ed3359cd6b06d1953d924efc57ed46798e6a64
Arthur Brant <arthurcl.br@hotmail.com>
2022-05-19 13:56:27 -0300

    center the author section

--- docs/.vitepress/theme/components/Author.vue
+++ docs/.vitepress/theme/components/Author.vue
@@ -3,7 +3,7 @@ import Socials from "./Socials.vue";
 </script>
 
 <template>
-    <div class="author flex flex-row gap-x-4">
+    <div class="author flex flex-row gap-x-4 justify-center">
         <div class="name ">Arthur Brant</div>
         <Socials class="socials flex flex-row gap-x-4 items-center" />
     </div>
